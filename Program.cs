27f04f57361a14f992842cb16c428cc7475ec46c
shivakumar <sk625700@gmail.com>
2021-11-10 20:09:57 +0530

    new changes

--- Program.cs
+++ Program.cs
@@ -8,13 +8,13 @@ namespace PropertiesDemo
         static void Main(string[] args)
         {
             //Console.WriteLine("Hello World!");
-            Employe employe = new Employe {Id=1001, Name="Ashok", Salary=9000};
+            Employe employe = new Employe {Id=1001, Name="KRISHNA", Salary=9000};
             Console.WriteLine(employe);
 
             List<Employe> employes = new List<Employe>
             {
-                new Employe {Id=1002,Name="Sujana", Salary=9700},
-                new Employe {Id=1003,Name="mounika", Salary=8000}
+                new Employe {Id=1002,Name="SHIVA", Salary=1000},
+                new Employe {Id=1003,Name="RAGHUU", Salary=8000}
             };
   foreach (Employe emp in employes)
     Console.WriteLine(emp);
